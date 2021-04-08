5d3119f42a1013d8a7f267988d6f60075e863538
Járlei Machado <jarlei_eng.mecanica@hotmail.com>
2021-04-08 17:50:03 -0300

    chore: SideBar component basic setup

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,5 +1,15 @@
+import { Flex } from '@chakra-ui/layout';
 import { Header } from '../components/Header';
+import { SideBar } from '../components/SideBar';
 
 export default function Dashboard() {
-  return <Header />;
+  return (
+    <Flex direction='column' h='100vh'>
+      <Header />
+
+      <Flex w='100%' my='6' maxWidth={1480} px='6'>
+        <SideBar />
+      </Flex>
+    </Flex>
+  );
 }
