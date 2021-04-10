0c247735bb374e5d7b9061453fc19a08f9b9d248
Járlei Machado <jarlei_eng.mecanica@hotmail.com>
2021-04-10 16:53:27 -0300

    feat: Added basic navigation flow

--- src/components/SideBar/NavLink.tsx
+++ src/components/SideBar/NavLink.tsx
@@ -1,19 +1,27 @@
-import Icon from '@chakra-ui/icon';
-import { Link, Text, LinkProps as ChakraLinkProps } from '@chakra-ui/react';
+import {
+  Icon,
+  Link as ChakraLink,
+  Text,
+  LinkProps as ChakraLinkProps,
+} from '@chakra-ui/react';
 import { ElementType, ReactNode } from 'react';
+import Link from 'next/link';
 
 interface NavLinkProps {
   icon: ElementType;
   children: string;
+  href: string;
 }
 
-export function NavLink({ icon, children, ...rest }: NavLinkProps) {
+export function NavLink({ icon, children, href, ...rest }: NavLinkProps) {
   return (
-    <Link display='flex' align='center' {...rest}>
-      <Icon as={icon} fontSize='20' />
-      <Text ml='4' fontWeight='medium'>
-        {children}
-      </Text>
+    <Link href={href} passHref>
+      <ChakraLink display='flex' align='center' {...rest}>
+        <Icon as={icon} fontSize='20' />
+        <Text ml='4' fontWeight='medium'>
+          {children}
+        </Text>
+      </ChakraLink>
     </Link>
   );
 }
