ef69db8ce1e849d35ba43ded3c6e5171a54a4c4e
Járlei Machado <jarlei_eng.mecanica@hotmail.com>
2021-04-10 09:22:41 -0300

    style: Added responsive sidebar

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,27 +1,44 @@
-import { Box, Stack } from '@chakra-ui/layout';
 import {
-  RiContactsLine,
-  RiDashboardLine,
-  RiGitMergeLine,
-  RiInputMethodLine,
-} from 'react-icons/ri';
-import { NavLink } from './NavLink';
-import { NavSection } from './NavSection';
+  Box,
+  useBreakpointValue,
+  Drawer,
+  DrawerContent,
+  DrawerOverlay,
+  DrawerCloseButton,
+  DrawerHeader,
+  DrawerBody,
+} from '@chakra-ui/react';
+import { useSideBarDrawer } from '../../contexts/SideBarDrawerContext';
+
+import { SideBarNav } from './SideBarNav';
 
 export function SideBar() {
+  const isDrawerSideBar = useBreakpointValue({
+    base: true,
+    lg: false,
+  });
+
+  if (isDrawerSideBar) {
+    const { isOpen, onClose } = useSideBarDrawer();
+
+    return (
+      <Drawer isOpen={isOpen} placement='left' onClose={onClose}>
+        <DrawerOverlay>
+          <DrawerContent bg='gray.800' p='4'>
+            <DrawerCloseButton mt='6' />
+            <DrawerHeader>Navegação</DrawerHeader>
+            <DrawerBody>
+              <SideBarNav />
+            </DrawerBody>
+          </DrawerContent>
+        </DrawerOverlay>
+      </Drawer>
+    );
+  }
+
   return (
     <Box as='aside' w='64' mr='8'>
-      <Stack spacing='12' align='flex-start'>
-        <NavSection title='GERAL'>
-          <NavLink icon={RiDashboardLine}>Dashboard</NavLink>
-          <NavLink icon={RiContactsLine}>Usuários</NavLink>
-        </NavSection>
-
-        <NavSection title='AUTOMAÇÃO'>
-          <NavLink icon={RiInputMethodLine}>Formulários</NavLink>
-          <NavLink icon={RiGitMergeLine}>Automação</NavLink>
-        </NavSection>
-      </Stack>
+      <SideBarNav />
     </Box>
   );
 }
