90e2be9fef58d618ba8c375510ab0ccb163255ab
Járlei Machado <jarlei_eng.mecanica@hotmail.com>
2021-04-27 09:28:45 -0300

    chore: Users listing

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -27,7 +27,19 @@ export default function UserList() {
     const response = await fetch('http://localhost:3000/api/users');
     const data = await response.json();
 
-    return data;
+    const users = data.users.map((user) => {
+      return {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
+          day: '2-digit',
+          month: 'long',
+          year: 'numeric',
+        }),
+      };
+    });
+    return users;
   });
 
   // console.log(query);
@@ -86,31 +98,37 @@ export default function UserList() {
                   </Tr>
                 </Thead>
                 <Tbody>
-                  <Tr>
-                    <Td px={['4', '4', '6']}>
-                      <Checkbox colorScheme='pink' />
-                    </Td>
-                    <Td>
-                      <Box>
-                        <Text fontWeight='bold'>Járlei Rodrigues</Text>
-                        <Text fontSize='sm' color='gray.300'>
-                          jarlei_rodrigues@outlook.com
-                        </Text>
-                      </Box>
-                    </Td>
-                    {isWideVersion && <Td>04 de Abril, 2021</Td>}
-                    <Td>
-                      {isWideVersion && (
-                        <Button
-                          as='a'
-                          size='sm'
-                          fontSize='sm'
-                          colorScheme='purple'
-                          leftIcon={<Icon as={RiPencilLine} fontSize='16' />}
-                        ></Button>
-                      )}
-                    </Td>
-                  </Tr>
+                  {data.map((user) => {
+                    return (
+                      <Tr key={user.id}>
+                        <Td px={['4', '4', '6']}>
+                          <Checkbox colorScheme='pink' />
+                        </Td>
+                        <Td>
+                          <Box>
+                            <Text fontWeight='bold'>{user.name}</Text>
+                            <Text fontSize='sm' color='gray.300'>
+                              {user.email}
+                            </Text>
+                          </Box>
+                        </Td>
+                        {isWideVersion && <Td>{user.createdAt}</Td>}
+                        <Td>
+                          {isWideVersion && (
+                            <Button
+                              as='a'
+                              size='sm'
+                              fontSize='sm'
+                              colorScheme='purple'
+                              leftIcon={
+                                <Icon as={RiPencilLine} fontSize='16' />
+                              }
+                            ></Button>
+                          )}
+                        </Td>
+                      </Tr>
+                    );
+                  })}
                 </Tbody>
               </Table>
 
